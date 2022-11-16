498fe828ff53de92fb55a32a1ed8762ee2596006
Martin <nicolas.martin@solera.com>
2022-11-16 16:05:05 +0100

    fetch

--- src/front/JSX/pages/Destinos/Destinos.jsx
+++ src/front/JSX/pages/Destinos/Destinos.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import "./Destinos.css"
-
-
+import React, { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import "./Destinos.css";
 
 const Destinos = () => {
+  const [destinations, setDestinations] = useState([]);
+
+  useEffect(() => {
+    getDestinations();
+  }, []);
+
+  const getDestinations = async () => {
+    const url = "https://741b-90-161-195-130.eu.ngrok.io/api/v1/destinations";
+    const options = {
+      method: "GET",
+      headers: new Headers({ "Content-type": "application/json" }),
+      mode: "no-cors",
+    };
+    const resp = await fetch(url, options);
+    const data = await resp.json();
+    setDestinations(data);
+    console.log(data);
+  };
+  
+
   return (
     <div>
-        <Navbar />
-        <div className='body align-items-center text-center justify-content-center'>
-            <div className='card card-container m-5 '>
-                <h1>BIENVENIDO A NUESTRA VENTANA DE DESTINOS</h1>
-            </div>
+      <Navbar />
+      <div className="body align-items-center text-center justify-content-center">
+        <div className="card card-container m-5 ">
+          {destinations.map(
+            dest => {
+              return<h1>{dest}</h1>
+            }
+          )}
         </div>
-  </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Destinos
+export default Destinos;
